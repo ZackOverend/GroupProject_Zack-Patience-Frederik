cd2cb2ca17db2f24faf37565eb27e6d1c0e7a7af
patience <gleaves@shernet.sheridancollege.ca>
2023-08-11 13:03:34 -0400

    set up recipe class and dietrecipe class

---
+++ RecipeFinder/BusinessLogic/DietRecipie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace RecipeFinder.BusinessLogic
+{
+    public class DietRecipe : Recipe
+    {
+
+        private bool _isVegetarian;
+
+        private bool _isVegan;
+
+        private bool _isDairyFree;
+
+        public bool IsVegetarian
+        {
+            get
+            {
+                return _isVegetarian;
+            }
+        }
+        public bool IsVegan
+        {
+            get
+            {
+                return _isVegan;
+            }
+        }
+
+        public bool IsDairyFree
+        {
+            get
+            {
+                return _isDairyFree;
+            }
+        }
+
+
+        public DietRecipe(int recipeId, string recipeDesc, string recipeName, string recipeImage, bool isVegetarian, bool isVegan, bool isDairyFree)
+            : base(recipeId, recipeDesc, recipeName, recipeImage)
+        {
+            _isVegetarian = isVegetarian;
+            _isVegan = isVegan;
+            _isDairyFree = isDairyFree;
+        }
+    }
+}
+
--- RecipeFinder/BusinessLogic/Recipe.cs
+++ RecipeFinder/BusinessLogic/Recipe.cs
@@ -1,11 +1,57 @@
 ﻿using System;
-namespace RecipeFinder.AppClasses
+namespace RecipeFinder.BusinessLogic
 {
-	public class Recipe
-	{
-		public Recipe()
-		{
-		}
-	}
+    public class Recipe
+    { 
+        private int _recipeId;
+        private string _recipeDesc;
+        private string _recipeName;
+        private string _recipeImage;
+        public int RecipeId { get { return _recipeId; } }
+
+        public string RecipeDesc
+        {
+            get
+            {
+                return _recipeDesc;
+            }
+            set
+            {
+                _recipeDesc = value;
+            }
+        }
+        public string RecipeName
+        {
+            get
+            {
+                return _recipeName;
+            }
+            set
+            {
+                _recipeName = value;
+            }
+        }
+        public string RecipeImage
+        {
+            get
+            {
+                return _recipeImage;
+            }
+            set
+            {
+                _recipeImage = value;
+            }
+        }
+
+
+        public Recipe(int recipeId, string recipeDesc, string recipeName, string recipeImage)
+        {
+            _recipeId = recipeId;
+            RecipeDesc = recipeDesc;
+            RecipeName = recipeName;
+            RecipeImage = recipeImage;
+
+        }
+    }
 }
 
