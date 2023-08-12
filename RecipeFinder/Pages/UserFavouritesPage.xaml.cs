f4f491a26c786fc4b14eb604f06ec7595a815de8
ZackOverend <zack.overend@outlook.com>
2023-08-12 15:50:35 -0400

    Implemented UserFavourites Page and View Recipe Page

--- RecipeFinder/Pages/UserFavouritesPage.xaml.cs
+++ RecipeFinder/Pages/UserFavouritesPage.xaml.cs
@@ -1,9 +1,50 @@
 ﻿namespace RecipeFinder.Pages;
+using RecipeFinder.BusinessLogic;
 
 public partial class UserFavouritesPage : ContentPage
 {
-	public UserFavouritesPage()
-	{
-		InitializeComponent();
-	}
+    private User _passedUser;
+
+    public User PassedUser { get { return _passedUser; } }
+
+    private Recipe _selectedFavourite;
+
+    public Recipe SelectedFavourite
+    {
+        get
+        {
+            return _selectedFavourite;
+        }
+        set
+        {
+            if (_selectedFavourite == value)
+                return;
+            _selectedFavourite = value;
+            OnPropertyChanged();
+        }
+    }
+    public UserFavouritesPage(User passedUser)
+    {
+        InitializeComponent();
+        _passedUser = passedUser;
+        BindingContext = this;
+    }
+
+    void ExportBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+
+    }
+
+    void RemoveFavouriteBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        PassedUser.UserFavourites.RemoveFavorite(_selectedFavourite);
+        Navigation.PopAsync();
+    }
+
+    void BackBtnFavourites_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+
 }
--- RecipeFinder/Pages/ViewRecipePage.xaml.cs
+++ RecipeFinder/Pages/ViewRecipePage.xaml.cs
@@ -1,9 +1,34 @@
-﻿namespace RecipeFinder.Pages;
+﻿using RecipeFinder.BusinessLogic;
+namespace RecipeFinder.Pages;
 
 public partial class ViewRecipePage : ContentPage
 {
-	public ViewRecipePage()
-	{
-		InitializeComponent();
-	}
+    private User _passedUser;
+    private Recipe _passedRecipe;
+
+    public User PassedUser { get { return _passedUser; } }
+    public Recipe PassedRecipe { get { return _passedRecipe; } }
+
+
+    public ViewRecipePage(User passedUser, Recipe passedRecipe)
+    {
+        InitializeComponent();
+        _passedUser = passedUser;
+        _passedRecipe = passedRecipe;
+        BindingContext = this;
+    }
+
+    void AddToFavourites_Clicked(System.Object sender, System.EventArgs e)
+    {
+        //Adding selected recipe to the list of favourites
+        PassedUser.UserFavourites.AddFavourite(PassedRecipe);
+
+        Navigation.PopAsync();
+    }
+
+    void GoBackBtn_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
 }
+
